9f87d6b046f1bb2297dd731df29a30db44ecdb51
Maria Loza <mloza@csumb.edu>
2017-02-03 05:36:48 +0000

    backing down

--- run.py
+++ run.py
@@ -1,34 +1,35 @@
 #run.py
 
-from flask import Flask
-from flask import request
+# from flask import Flask
+# from flask import request
 import flask 
-import tweepy
+# import tweepy
 import os
-import requests, json
-import random
+# import requests, json
+# import random
 
 app = flask.Flask(__name__)
 
-CONSUMER_KEY = '7jLLLL9umXTzcehteOGYi64DB'
-CONSUMER_SECRET_KEY = 'X3Rnv5oi6nc1P5wLWsgDRiKbi0rYhrv8V2bfG20CKaml4MaYpt'
+# CONSUMER_KEY = '7jLLLL9umXTzcehteOGYi64DB'
+# CONSUMER_SECRET_KEY = 'X3Rnv5oi6nc1P5wLWsgDRiKbi0rYhrv8V2bfG20CKaml4MaYpt'
 
-ACCESS_TOKEN = '1518510956-T4xcbHXHnqokR75OJCtxqwhLHal4mSswfVfmstL'
-ACCESS_TOKEN_SECRET = 'EiZTFhSXUAFhhSrslbfH6nHygVIcGjFaKFClVU5arOUk4'
+# ACCESS_TOKEN = '1518510956-T4xcbHXHnqokR75OJCtxqwhLHal4mSswfVfmstL'
+# ACCESS_TOKEN_SECRET = 'EiZTFhSXUAFhhSrslbfH6nHygVIcGjFaKFClVU5arOUk4'
 
 
 @app.route('/')  # Google "Python decorator" 
 def index():
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
+    # auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     
-    api = tweepy.API(auth)
+    # api = tweepy.API(auth)
     
-    results = api.search(q="sushi", lang="en")
+    # results = api.search(q="sushi", lang="en")
     
-    results = tweepy.Cursor(api.search, q="sushi", lang="en").items(1)
+    # results = tweepy.Cursor(api.search, q="sushi", lang="en").items(1)
       
-    return flask.render_template('twitterTest.html', public_tweets=results)
+    # return flask.render_template('twitterTest.html', public_tweets=results)
+    return "Hi"
     
 
 app.run(
