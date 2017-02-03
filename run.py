9d48ac9946f737d6beb293aeb8ce56c78c50169c
Maria Loza <mloza@csumb.edu>
2017-02-03 07:41:33 +0000

    added getty and css

--- run.py
+++ run.py
@@ -12,19 +12,27 @@ app = flask.Flask(__name__)
 
 @app.route('/')  # Google "Python decorator" 
 def index():
-    # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
-    # auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    ###### GETTY API image setup
+    url = "https://api.gettyimages.com/v3/search/images?fields=id,title,comp,referral_destinations&sort_order=best&phrase=sushi"
+    my_headers = { "Api-Key": os.getenv("GETTY_KEY") }
+    response = requests.get(url, headers=my_headers)
+    json_body = response.json()
+    pic = json_body["images"][random.randrange(0, 30)]["display_sizes"][0]["uri"]
+    # print pic
     
+    
+    ###### TWITTER/TWEEPY API setup
     auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET_KEY"))
     auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
-    
+    # auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
+    # auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     api = tweepy.API(auth)
-    
     results = api.search(q="sushi", lang="en")
-    
     results = tweepy.Cursor(api.search, q="sushi", lang="en").items(1)
+    
+    
       
-    return flask.render_template('twitterTest.html', public_tweets=results)
+    return flask.render_template('twitterTest.html', public_tweets=results, bg_image=pic)
     # return "Hi"
     
 
