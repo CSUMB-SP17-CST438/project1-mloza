5e6131545885d5cc6f72660664f2811541131900
Maria Loza <mloza@csumb.edu>
2017-02-03 08:16:45 +0000

    clean and presentable site

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ How searches reflect theme?:
 The quotes contain the keyword sushi and the images have some presence of sushi as well.
 
 Problems?:
+I wasn't sure where to start. I started a few days ago (before Thursday's lecture), and it felt like being thrown into the deep end. 
+There was a lot of imformation of the Twitter api and not enough. I wasn't sure how to set it up. After multiple try and errors, it started working.
+However, after many frustrations, I had added an empty space in my secret key. The site started working immediately after that.
+I was also very confused on how to access the config vars.
+After Thursday's lecture, it all made sense.
+
+Improvements?:
+Relax more. I was stressing out that I haven't done much with such little time. Seek out help instead after many attempts of Google.
--- run.py
+++ run.py
@@ -8,40 +8,28 @@ import os
 import requests, json
 import random
 
-CONSUMER_KEY = '7jLLLL9umXTzcehteOGYi64DB'
-CONSUMER_SECRET_KEY = 'X3Rnv5oi6nc1P5wLWsgDRiKbi0rYhrv8V2bfG20CKaml4MaYpt'
-
-ACCESS_TOKEN = '1518510956-T4xcbHXHnqokR75OJCtxqwhLHal4mSswfVfmstL'
-ACCESS_TOKEN_SECRET = 'EiZTFhSXUAFhhSrslbfH6nHygVIcGjFaKFClVU5arOUk4'
-GETTY_KEY = 'tt9qedn4c4p439qn4w7rkjy3'
-GETTY_SECRET_KEY = 'xVmHYqk8nu853DNMneNJVv7x245Y4tk8aHGaxS7bRNRep'
-
 app = flask.Flask(__name__)
 
 @app.route('/')  # Google "Python decorator" 
 def index():
     ###### GETTY API image setup
     url = "https://api.gettyimages.com/v3/search/images?fields=id,title,comp,referral_destinations&sort_order=best&phrase=sushi"
-    my_headers = { "Api-Key": GETTY_KEY }
+    my_headers = { "Api-Key": os.getenv("GETTY_KEY") }
     response = requests.get(url, headers=my_headers)
     json_body = response.json()
     pic = json_body["images"][random.randrange(0, 30)]["display_sizes"][0]["uri"]
-    # print pic
-    
+
     
     ###### TWITTER/TWEEPY API setup
-    # auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET_KEY"))
-    # auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    auth = tweepy.OAuthHandler(os.getenv("CONSUMER_KEY"), os.getenv("CONSUMER_SECRET_KEY"))
+    auth.set_access_token(os.getenv("ACCESS_TOKEN"), os.getenv("ACCESS_TOKEN_SECRET"))
     api = tweepy.API(auth)
     results = api.search(q="sushi", lang="en")
     results = tweepy.Cursor(api.search, q="sushi", lang="en").items(1)
     
     
       
-    return flask.render_template('twitterTest.html', public_tweets=results, bg_image=pic)
-    # return "Hi"
+    return flask.render_template('proj1.html', public_tweets=results, bg_image=pic)
     
 
 app.run(
